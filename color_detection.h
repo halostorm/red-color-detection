4a38946278a02b5a638eb11e920e249686afe694
halostorm <2524416894@qq.com>
2017-12-28 00:11:47 +0800

    adjust center

--- color_detection.h
+++ color_detection.h
@@ -3,15 +3,20 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/video.hpp>
+#include <ctime>
 
 using namespace std;
 using namespace cv;
 
 //For Test Purpose , it extract only the "red color" ,it may detect orange also :) (no escape from that) 
 // return: a binary image where the extracted color represent '1' and other are 0
-void ExtractColoredObjects(Mat & _src,Mat & _outputMat)
-{
+void ExtractColoredObjects(Mat & _src, Mat & _outputMat) {
 	//Opencv see BGR code via Scalar Function 
 	//TO Change it to another color , use the numbers range from http://www.workwithcolor.com/red-color-hue-range-01.htm
 	//But remember i use the HSV plane instead of BGR so you need to convert HSV to BGR.
@@ -20,13 +25,13 @@ void ExtractColoredObjects(Mat & _src,Mat & _outputMat)
 	//the code is a snippest from stackoverflow after many tries of me that was work sometimes and sometimes not. :D
 	//http://stackoverflow.com/questions/32522989/opencv-better-detection-of-red-color
 	Mat hsv_src;
-    cvtColor(_src, hsv_src, COLOR_BGR2HSV);
+	cvtColor(_src, hsv_src, COLOR_BGR2HSV);
 
-    Mat mask1, mask2;
-    inRange(hsv_src, Scalar(0, 70, 50), Scalar(10, 255, 255), mask1);
-    inRange(hsv_src, Scalar(170, 70, 50), Scalar(180, 255, 255), mask2);
+	Mat mask1, mask2;
+	inRange(hsv_src, Scalar(0, 100, 100), Scalar(10, 255, 255), mask1);
+	inRange(hsv_src, Scalar(170, 100, 100), Scalar(180, 255, 255), mask2);
 
-    _outputMat = mask1 | mask2;
+	_outputMat = mask1 | mask2;
 
 }
 
@@ -34,47 +39,59 @@ void ExtractColoredObjects(Mat & _src,Mat & _outputMat)
 //_originalImage : original colored image
 // return the _originalImage(colored) + rectangle bounary around the non zero pixels
 // return the avg x,y of this rectangle
-void drawBoundaryAroundObjects(Mat & _src,Mat & _originalImage,Mat & _outputMat,Point & _avgPos)
-{
+void drawBoundaryAroundObjects(Mat & _src, Mat & _originalImage,
+		Mat & _outputMat, Point & _avgPos) {
 	//This is just a cheap way and not efficient for multiple objects
-    cv::Size size = _src.size();
 
+	cv::Size size = _src.size();
+	cout << "binaryFrame size:" << size.width << "\t" << size.height << endl;
 	// The bounding rectangle is defined by its top-left (TL) and bottom-right (BR)
-    // coordinates.
-    cv::Point tl( size.width, size.height );
-    cv::Point br(          0,           0 );
-    bool hasPoints = false;
+	// coordinates.
+	int center_x = 0;
+	int center_y = 0;
+	cv::Point tl(size.width, size.height);
+	cv::Point br(0, 0);
+	bool hasPoints = false;
+	long point_i = 0;
+	for (int y = 0; y < size.height; ++y) {
+		for (int x = 0; x < size.width; ++x) {
+			if (_src.at<unsigned char>(y, x) > 0) {
+				hasPoints = true;
 
-    for ( int y = 0; y < size.height; ++y )
-    {
-        for ( int x = 0; x < size.width; ++x )
-        {
-            if ( _src.at<unsigned char>( y, x ) > 0 )
-            {
-                hasPoints = true;
+				// Update the top-left corner.
+				if (x < tl.x)
+					tl.x = x;
+				if (y < tl.y)
+					tl.y = y;
 
-                // Update the top-left corner.
-                if ( x < tl.x ) tl.x = x;
-                if ( y < tl.y ) tl.y = y;
+				// Update the bottom-right corner.
+				if (x > br.x)
+					br.x = x;
+				if (y > br.y)
+					br.y = y;
+				center_x = (center_x * point_i + x) / (point_i + 1);
+				center_y = (center_y * point_i + y) / (point_i + 1);
+				point_i++;
 
-                // Update the bottom-right corner.
-                if ( x > br.x ) br.x = x;
-                if ( y > br.y ) br.y = y;
-            }
-        }
-    }
+			}
+		}
+	}
 
-    _outputMat = _originalImage;
-    // If the image contains any non-zero pixels, then draw green rect box with thickness of 5
-    if ( hasPoints )
-    {
-        cv::rectangle( _outputMat, tl, br, Scalar( 0, 255, 0), 5 );
-        _avgPos.x = ( tl.x + br.x ) / 2;
-        _avgPos.y = ( tl.y + br.y) / 2;
-    }
-    else
-    	putText(_outputMat,"No Red Color ?",Point(0,60),2,2,Scalar(0,0,255));
+	_outputMat = _originalImage;
+	// If the image contains any non-zero pixels, then draw green rect box with thickness of 5
+	if (hasPoints) {
+		tl.x = center_x - 10;
+		tl.y = center_y - 10;
+		br.x = center_x + 10;
+		br.y = center_y + 10;
+		cv::rectangle(_outputMat, tl, br, Scalar(0, 255, 0), 5);
+		//_avgPos.x = ( tl.x + br.x ) / 2;
+		//_avgPos.y = ( tl.y + br.y) / 2;
+		_avgPos.x = center_x;
+		_avgPos.y = center_y;
+	} else
+		putText(_outputMat, "No Red Color ?", Point(0, 60), 2, 2,
+				Scalar(0, 0, 255));
 
 }
-
 
--- mainTests.cpp
+++ mainTests.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <sstream>
 #include "color_detection.h"
- 
+
 #define DISPLAY_WIDTH	640
 #define DISPLAY_HEIGHT	480
 
@@ -9,82 +10,94 @@ using namespace cv;
 
 Point g_avgRectPos;
 
-void Test_image(int argc,char **argv)
-{
+void Test_image(int argc, char **argv) {
 
-	if(argc != 2)
-	{
-		cout << "provide an image" <<endl;
+	if (argc != 2) {
+		cout << "provide an image" << endl;
 		return;
 	}
 
+	Mat inputImage, binaryImage, extractedObjectsImage;
 
-	Mat inputImage,binaryImage,extractedObjectsImage;
+	inputImage = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-	inputImage = imread( argv[1], CV_LOAD_IMAGE_COLOR);
+	ExtractColoredObjects(inputImage, binaryImage);
+	drawBoundaryAroundObjects(binaryImage, inputImage, extractedObjectsImage,
+			g_avgRectPos);
 
-	ExtractColoredObjects( inputImage, binaryImage);
-	drawBoundaryAroundObjects(binaryImage,inputImage,extractedObjectsImage,g_avgRectPos);
+	cout << "middle Red Rectangle location[x,y]: " << "[ " << g_avgRectPos.x
+			<< " , " << g_avgRectPos.y << " ]" << endl;
 
-	cout << "middle Red Rectangle location[x,y]: " <<"[ "<< g_avgRectPos.x << " , "<< g_avgRectPos.y << " ]" <<endl;
+	namedWindow("src_image", 0);
+	namedWindow("binary_image", 0);
+	namedWindow("withObjects", 0);
 
-	namedWindow("src_image",0);
-	namedWindow("binary_image",0);
-	namedWindow("withObjects",0);
+	resizeWindow("src_image", DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	resizeWindow("binary_image", DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	resizeWindow("withObjects", DISPLAY_WIDTH, DISPLAY_HEIGHT);
 
- 	resizeWindow( "src_image", DISPLAY_WIDTH, DISPLAY_HEIGHT);
- 	resizeWindow( "binary_image", DISPLAY_WIDTH, DISPLAY_HEIGHT);
-	resizeWindow( "withObjects", DISPLAY_WIDTH, DISPLAY_HEIGHT);
-
-	imshow( "src_image", inputImage);
-	imshow( "binary_image", binaryImage);
-	imshow ("withObjects",extractedObjectsImage);
+	imshow("src_image", inputImage);
+	imshow("binary_image", binaryImage);
+	imshow("withObjects", extractedObjectsImage);
 
 	waitKey(0);
 }
 
-void Test_video()
-{
+int Test_video() {
 	VideoCapture cap;
 	Mat frame;
-	Mat binaryFrame,extractedObjectsFrame;
+	Mat binaryFrame, extractedObjectsFrame;
 	bool frameReadSuccess;
 	cap.open(0);
 
 	if (!cap.isOpened())  // if not success, exit program
 	{
 		cout << "ERROR INITIALIZING VIDEO CAPTURE" << endl;
-		return;
+		return -1;
 	}
 
-	namedWindow("outputFrame",0);
-	namedWindow("binaryFrame",0);
-	while(1)
-	{
+	namedWindow("outputFrame", 0);
+	namedWindow("binaryFrame", 0);
+	int index = 0;
+	while (1) {
+
 		frameReadSuccess = cap.read(frame);
-		if(frameReadSuccess == false)
-		{
-			cout<<"ERROR READING FRAME FROM CAMERA FEED"<<endl;
+		if (frameReadSuccess == false) {
+			cout << "ERROR READING FRAME FROM CAMERA FEED" << endl;
 			break;
 		}
 
-		ExtractColoredObjects( frame, binaryFrame);
-		drawBoundaryAroundObjects(binaryFrame,frame,extractedObjectsFrame,g_avgRectPos);
+		ExtractColoredObjects(frame, binaryFrame);
+		drawBoundaryAroundObjects(binaryFrame, frame, extractedObjectsFrame,
+				g_avgRectPos);
 
-		imshow("outputFrame",extractedObjectsFrame);
-		imshow("binaryFrame",binaryFrame);
+		imshow("outputFrame", extractedObjectsFrame);
+		imshow("binaryFrame", binaryFrame);
 
-		cout << "middle Red Rectangle location[x,y]: " <<"[ "<< g_avgRectPos.x << " , "<< g_avgRectPos.y << " ]" <<endl;
-		waitKey(30);
-
+		cout << "middle Red Rectangle location[x,y]: " << "[ " << g_avgRectPos.x
+				<< " , " << g_avgRectPos.y << " ]" << endl;
+		char key = waitKey(30);
+		if (key == 27) {
+			return 0;
+		}
+		if (key == 32) {
+			stringstream ss;
+			string file_tile = "../save";
+			string file_index;
+			string file_format = ".jpg";
+			ss << index;
+			ss >> file_index;
+			string file_name = file_tile+file_index+file_format;
+			imwrite(file_name, extractedObjectsFrame);
+		}
+		index++;
 	}
-	
+
 }
 
-int main (int argc,char ** argv)
-{
+int main(int argc, char ** argv) {
 	//choose which test to apply
-//	Test_image(argc,argv);
+	//Test_image(argc,argv);
 	Test_video();
 
 	return 0;
